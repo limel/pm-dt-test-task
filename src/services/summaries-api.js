555bbb669cdc77f71d8891159d4a57df41569c59
limelaudio <limelaudio@gmail.com>
2022-01-31 00:40:03 +0200

    edit api options

--- src/services/summaries-api.js
+++ src/services/summaries-api.js
@@ -2,18 +2,25 @@ import axios from 'axios';
 
 axios.defaults.baseURL = 'http://135.181.30.244:27007/api';
 
+const options = {
+  headers: {
+    'Access-Control-Allow-Origin': '*',
+    'Content-Type': 'application/json',
+  },
+}
+
 export async function fetchSummaries() {
-  const { data } = await axios.get(`/summaries/`);
+  const { data } = await axios.get(`/summaries/`, options);
   return data;
 }
 
 export async function fetchSummariesPage(page) {
-  const { data } = await axios.get(`/summaries/?page=${page}`);
+  const { data } = await axios.get(`/summaries/?page=${page}`, options);
   return data;
 }
 
 export async function fetchSummariesWithPhoto(param) {
-  const { data } = await axios.get(`/summaries/?photo=${param}`);
+  const { data } = await axios.get(`/summaries/?photo=${param}`, options);
   return data;
 }
 
